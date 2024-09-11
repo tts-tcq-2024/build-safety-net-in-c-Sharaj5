3ba3500f157fbe08a35d6c804172132d5b1dfc93
Sharaj5 <ssharaj2@gmail.com>
2024-09-11 14:41:47 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -4,16 +4,62 @@
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
-  generateSoundex("AX", soundex);
-  generateSoundex("HARI", soundex);
-  generateSoundex("CHAI", soundex);
-  generateSoundex("JOY", soundex);
-  generateSoundex("HI", soundex);
-  generateSoundex("HARSH", soundex);
-  generateSoundex("SHAR", soundex);
-  generateSoundex("WU", soundex);
-  generateSoundex("BC", soundex);
-  generateSoundex("DASH", soundex);
- 
- // ASSERT_EQ(soundex,"A200");
+  generateSoundex("AB", soundex);
+  EXPECT_STREQ(soundex,"A100");
+}
+
+TEST(SoudexTestsuite, testcase1) {
+ //AAA
+  char soundex[5];
+  generateSoundex("ABXY", soundex);
+  EXPECT_STREQ(soundex,"A120");
+}
+
+TEST(SoudexTestsuite, testcase2) {
+ //AAA
+  char soundex[5];
+  generateSoundex("CDLM", soundex);
+  EXPECT_STREQ(soundex,"C345");
+}
+
+TEST(SoudexTestsuite, testcase3) {
+ //AAA
+  char soundex[5];
+  generateSoundex("PJAY", soundex);
+  EXPECT_STREQ(soundex,"P200");
+}
+
+
+TEST(SoudexTestsuite, testcase4) {
+ //AAA
+  char soundex[5];
+  generateSoundex("PJAY", soundex);
+  EXPECT_STREQ(soundex,"P200");
+}
+
+TEST(SoudexTestsuite, testcase5) {
+ //AAA
+  char soundex[5];
+  generateSoundex("BPDL", soundex);
+  EXPECT_STREQ(soundex,"B134");
+}
+
+TEST(SoudexTestsuite, testcase6) {
+ //AAA
+  char soundex[5];
+  generateSoundex("U", soundex);
+  EXPECT_STREQ(soundex,"U000");
+}
+TEST(SoudexTestsuite, testcase7) {
+ //AAA
+  char soundex[5];
+  generateSoundex("VM", soundex);
+  EXPECT_STREQ(soundex,"V500");
+}
+
+TEST(SoudexTestsuite, testcase8) {
+ //AAA
+  char soundex[5];
+  generateSoundex("AEI", soundex);
+  EXPECT_STREQ(soundex,"A000");
 }
